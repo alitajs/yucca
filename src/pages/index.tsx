f9c74b5b9e0771d5de6e96a717da367b819805a6
wangxingkang <wangxingkang@sensoro.com>
2019-10-28 10:36:36 +0800

    feat: 添加title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import BasicLayout from '@/layouts/basic';
+import Helmet from '@/components/Helmet';
 
 const IndexPage: React.FC = () => {
   return (
     <div>
+      <Helmet title="页面编辑器" />
       <BasicLayout />
     </div>
   )
